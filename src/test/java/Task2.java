25b2822f071c80b6c411375030af2dbe90bd0d3d
Tinatin Gvelesiani <tinatingvelesiani@credo.ge>
2024-10-17 23:30:55 +0400

    Task2

---
+++ src/test/java/Task2.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+
+public class Task2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("შეიყვანეთ სიმბოლო:");
+            char symbol = scanner.next().charAt(0);
+            switch (symbol) {
+                case 'ა':
+                case 'ე':
+                case 'ი':
+                case 'ო':
+                case 'უ':
+                    System.out.println("სიმბოლო " + symbol + " არის ხმოვანი");
+                    break;
+                case 'ბ':
+                case 'გ':
+                case 'დ':
+                case 'ვ':
+                case 'ზ':
+                case 'თ':
+                case 'კ':
+                case 'ლ':
+                case 'მ':
+                case 'ნ':
+                case 'პ':
+                case 'ჟ':
+                case 'რ':
+                case 'ს':
+                case 'ტ':
+                case 'ფ':
+                case 'ქ':
+                case 'ღ':
+                case 'ყ':
+                case 'შ':
+                case 'ჩ':
+                case 'ც':
+                case 'ძ':
+                case 'წ':
+                case 'ჭ':
+                case 'ხ':
+                case 'ჯ':
+                case 'ჰ':
+                    System.out.println("სიმბოლო " + symbol + " არის თანხმოვანი");
+                    break;
+                default:
+                    System.out.println("შეიყვანეთ სწორი სიმბოლო!");
+            }
+
+    }
+}
